54a290e83564b4d265a0e644c400708dea8fd922
Lucas <lucaslou4@gmail.com>
2020-02-06 19:42:20 -0300

    fix: remove conflicting keybinds

--- config.h
+++ config.h
@@ -198,12 +198,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_U,           kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_D,           kscrolldown,    {.i = -1} },
-	{ ShiftMask,            XK_K,           kscrollup,      {.i =  1} },
-	{ ShiftMask,            XK_J,           kscrolldown,    {.i =  1} },
+	{ ControlMask,          XK_k,           kscrollup,      {.i =  1} },
+	{ TERMMOD,              XK_K,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_J,           kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_j,           kscrolldown,    {.i =  1} },
 };
 
 /*
