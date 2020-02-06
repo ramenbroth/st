6e7fcb225e7d868e9231bd4e2b3c6c92c20b98ae
Lucas <lucaslou4@gmail.com>
2020-02-05 23:36:54 -0300

    feat: add keybindings for scrolling and zooming

--- config.h
+++ config.h
@@ -189,8 +189,9 @@ static Shortcut shortcuts[] = {
 	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
 	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
 	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
-	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
+	{ TERMMOD,              XK_plus,        zoom,           {.f = +1} },
+	/* if shift is pressed - turns into _ */
+	{ TERMMOD,              XK_underscore,       zoom,      {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
 	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
 	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
@@ -199,6 +200,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_U,           kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_D,           kscrolldown,    {.i = -1} },
+	{ ShiftMask,            XK_K,           kscrollup,      {.i =  1} },
+	{ ShiftMask,            XK_J,           kscrolldown,    {.i =  1} },
 };
 
 /*
