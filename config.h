745181ba8245ce6def06a453c2dbe5a417e25834
Lucas Vasconcelos <lucaslou4@protonmail.com>
2020-07-06 10:02:11 -0300

    swap rofi for dmenu

--- config.h
+++ config.h
@@ -139,7 +139,7 @@ static MouseShortcut mshortcuts[] = {
 };
 
 static char *openurlcmd[] = { "/bin/sh", "-c",
-    "sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)'| uniq | sed 's/^www./http:\\/\\/www\\./g' | rofi -dmenu -i -p 'Follow which url?' | xargs -r xdg-open",
+    "sed 's/.*│//g' | tr -d '\n' | grep -aEo '(((http|https)://|www\\.)[a-zA-Z0-9.]*[:]?[a-zA-Z0-9./&%?$#=_-]*)|((magnet:\\?xt=urn:btih:)[a-zA-Z0-9]*)'| uniq | sed 's/^www./http:\\/\\/www\\./g' | dmenu -i -p 'Follow which url?' | xargs -r xdg-open",
     "externalpipe", NULL };
 
 /* Internal keyboard shortcuts. */
